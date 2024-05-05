e0564f5ddfd7aa464471ea75ec5b14f856269ae2
Andrew <121280646+andreixyzf@users.noreply.github.com>
2024-05-05 18:01:09 +0300

    Update set.lua

--- nvim/lua/andrei/set.lua
+++ nvim/lua/andrei/set.lua
@@ -8,3 +8,45 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.clipboard = "unnamedplus"
+
+-- Autocomplete --
+vim.opt.completeopt = "menuone", "longest"
+vim.opt.complete = "kspell"
+
+-- Set the ColorScheme --
+vim.opt.background = "dark"
+
+require("colors/gruvbox").load()
+-- require("colors/gruvbox_spec").load() --
+
+function Colors(color)
+    color = color or "gruvbox"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+end
+
+Colors()
+
+-- Remove all trailing white space after saving --
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*" },
+    command = [[%s/\s\+$//e]],
+})
+
+
+vim.opt.cursorcolumn = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = false
